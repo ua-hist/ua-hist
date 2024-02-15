2cd736dbe49f539d0ce40015b0fbd35e3be5655e
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-02-15 20:53:04 +0200

    client: add events list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,37 @@ import "leaflet/dist/leaflet.css";
 import "leaflet";
 import "./App.scss";
 import { MapSection } from "./components/MapSection";
+import { useEffect, useState } from "react";
+import { HistoryEvent, getAllEvents } from "./api/api";
 
 function App() {
+  const [events, setEvents] = useState<HistoryEvent[]>([]);
+
+  useEffect(() => {
+    getAllEvents().then((res) => {
+      setEvents(res);
+    });
+  }, []);
+
   return (
     <div className="main">
       <div className="map">
         <MapSection />
+      </div>
+      <div className="events_list">
+        {events.map((event) => (
+          <div className="event" key={event.id}>
+            <div className="event_time">
+              <div>{event.time}</div>
+            </div>
+            <div
+              className="event_desc"
+              dangerouslySetInnerHTML={{
+                __html: event.eventsMarkup,
+              }}
+            ></div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ client/src/api/api.ts
@@ -0,0 +1,24 @@
+const apiUrl = "http://localhost:3000";
+
+export type HistoryEvent = {
+  id: string;
+  time: string;
+  events: string;
+  eventsMarkup: string;
+  eventIndex: number;
+};
+
+const replaceLinks = (record: HistoryEvent): HistoryEvent => {
+  const markup = record.eventsMarkup.replace(
+    new RegExp(`href="/wiki`, "g"),
+    `href="https://uk.wikipedia.org/wiki`
+  );
+
+  return { ...record, eventsMarkup: markup };
+};
+
+export const getAllEvents = async (): Promise<HistoryEvent[]> => {
+  return fetch(apiUrl + "/events")
+    .then((res) => res.json())
+    .then((records: HistoryEvent[]) => records.map(replaceLinks));
+};
