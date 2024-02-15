e741ad39e2db8871053d10932496c9e7387117ea
kkpagaev <kkpagaev@gmail.com>
2024-02-15 13:27:10 +0200

    moved build to a file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,44 +1,4 @@
-import Fastify, { FastifyZod } from "fastify";
-import fastifyCors from "@fastify/cors";
-import prismaPlugin from "./plugins/prisma";
-import { Prisma } from "@prisma/client";
-import { z } from "zod";
-import { validatorCompiler, serializerCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
-
-async function build(): Promise<FastifyZod> {
-  const f = Fastify();
-  await f.register(fastifyCors, {
-    origin: "*",
-  });
-  await f.register(prismaPlugin, {
-    databaseUrl: "postgresql://user:user@localhost:6262/user?schema=public",
-  });
-
-  f.setValidatorCompiler(validatorCompiler);
-  f.setSerializerCompiler(serializerCompiler);
-  f.setErrorHandler(async (err, _req, rep) => {
-    if (err instanceof z.ZodError) {
-      return rep.status(400).send({
-        message: "Invalid request",
-        issues: err.issues,
-      });
-    }
-    if (err instanceof Prisma.PrismaClientKnownRequestError) {
-      const code = err.code;
-
-      // not found
-      if (code === "P2018" || code === "P2025") {
-        return rep.status(404).send({
-          message: "Not found",
-        });
-      }
-    }
-
-    return rep.status(500).send(err.message);
-  });
-
-  return f.withTypeProvider<ZodTypeProvider>();
-}
+import { build } from "./build";
 
 async function main() {
   const f = await build();
