25724851d433fb98902d095b638525b2002a3b58
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-03-01 09:53:34 +0200

    separate sidebar controls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,23 +3,14 @@ import "leaflet";
 import "./App.scss";
 import { MapSection } from "./components/MapSection";
 import { useEffect, useState } from "react";
-import { DateInput } from "./components/DateInput";
 import { TimeLineList } from "./components/TimeLineList";
 import { StorageHelper } from "./utils/storage";
-
-const mapModes = ["europe", "ukraine"] as const;
-
-export type MapDisplayMode = (typeof mapModes)[number];
-
-const mapModesDict: Record<MapDisplayMode, string> = {
-  europe: "Europe",
-  ukraine: "Ukraine and neigbors",
-};
+import { MapDisplayMode, SidebarControls } from "./components/SidebarControls";
 
 function App() {
   const [date, setDate] = useState<number>(StorageHelper.get("date", 900));
   const [mapMode, setMapMode] = useState<MapDisplayMode>(
-    StorageHelper.get("mapMode", "europe")
+    StorageHelper.get("mapMode", "europe"),
   );
 
   useEffect(() => {
@@ -36,28 +27,12 @@ function App() {
         <MapSection mapMode={mapMode} date={date} />
       </div>
       <div className="sidebar">
-        <div className="sidebar_top">
-          <div className="flex flex-col">
-            <DateInput date={date} setDate={setDate} />
-            <div className="flex flex-row">
-              <div>Map scope: </div>
-              <select
-                value={mapMode}
-                onChange={(e) => {
-                  const mode = e.target.value as unknown as MapDisplayMode;
-                  setMapMode(mode);
-                  StorageHelper.set("mapMode", mode);
-                }}
-              >
-                {mapModes.map((mode) => (
-                  <option key={mode} value={mode}>
-                    {mapModesDict[mode]}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-        </div>
+        <SidebarControls
+          date={date}
+          setDate={setDate}
+          mapMode={mapMode}
+          setMapMode={setMapMode}
+        />
 
         <TimeLineList setDate={setDate} />
       </div>
