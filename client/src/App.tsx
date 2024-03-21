195f16db86a2c220936c4e3137d8dd8571fd0d03
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-03-21 22:00:44 +0200

    client: separate main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,14 @@
 import "leaflet/dist/leaflet.css";
 import "leaflet";
 import "./App.scss";
-import { MapSection } from "./components/maps/MapSection";
-import { useEffect, useState } from "react";
-import { TimeLineList } from "./components/timeline/TimeLineList";
-import {
-  ResizablePanelGroup,
-  ResizablePanel,
-  ResizableHandle,
-} from "./components/ui/resizable";
 import { Navbar } from "./components/Navbar";
+import { Main } from "./components/Main";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel>
-          <div className="map">
-            <MapSection />
-          </div>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel maxSize={50} minSize={20} defaultSize={30}>
-          <div className="sidebar">
-            <TimeLineList />
-          </div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+      <Main />
     </div>
   );
 }
--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,3 +1,25 @@
+import { MapSection } from "../components/maps/MapSection";
+import { TimeLineList } from "../components/timeline/TimeLineList";
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "../components/ui/resizable";
+
 export function Main() {
-  return <></>;
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel>
+        <div className="map">
+          <MapSection />
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel maxSize={50} minSize={20} defaultSize={30}>
+        <div className="sidebar">
+          <TimeLineList />
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
 }
