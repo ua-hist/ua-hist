f69cf4eed070b327640c27c3507e4ee05f50278e
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-03-22 11:04:37 +0200

    add link to saved

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { GearIcon } from "@radix-ui/react-icons";
 import { SettingsControls } from "./settings/SettingsControls";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Button } from "./ui/button";
+import { NavLink } from "react-router-dom";
 
 export function SettingsPopover() {
   return (
@@ -24,17 +25,22 @@ export function SettingsPopover() {
 
 export function Navbar() {
   return (
-    <div className="px-5 py-3 border-black  shadow-[#b6b6b6] shadow-md bg-slate-50 fixed z-10 top-2 left-14 rounded-lg">
+    <div className="px-5 py-3 border-black shadow-[#b6b6b6] shadow-md bg-slate-50 fixed z-10 top-2 left-14 rounded-lg">
       <div className="flex justify-between align-center gap-10">
         <div className="flex flex-row gap-4">
           <div className="flex flex-row justify-center items-center">
             <img className="w-10 h-10" src={uaIcon} alt="ua_icon_logo" />
           </div>
+
           <div className="flex flex-row justify-center items-center">
             <div className="text-lg font-medium text-slate-800">
               Ukraine History Atlas
             </div>
           </div>
+        </div>
+
+        <div className="flex flex-row justify-center items-center">
+          <NavLink to={"/saved"}>Saved events</NavLink>
         </div>
         <div>
           <SettingsPopover />
