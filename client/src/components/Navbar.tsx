a42aeeb833f814f06f3735a689ac9c01bfe34a06
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-04-04 16:05:49 +0300

    move auth dialog

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,11 @@
 import uaIcon from "../assets/ua_icon.png";
 import { Button } from "./ui/button";
 import { useTranslation } from "react-i18next";
-
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
-import {
-  Card,
-  CardHeader,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { SavedEventsPage } from "./saved-events/SavedEventsPage";
 import { SettingsPopover } from "./settings/SettingsPopover";
+import { SignUpLoginDialog } from "./auth/SignupLoginDialog";
 
 export function Navbar() {
   const { t } = useTranslation();
@@ -57,6 +47,7 @@ export function Navbar() {
                   </span>
                 </Button>
               </DialogTrigger>
+
               <DialogContent>
                 <SignUpLoginDialog />
               </DialogContent>
@@ -68,59 +59,5 @@ export function Navbar() {
         </div>
       </div>
     </div>
-  );
-}
-
-function SignUpLoginDialog() {
-  const { t } = useTranslation();
-
-  return (
-    <Tabs defaultValue="signup">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="signup">{t(`auth.sign_up`)}</TabsTrigger>
-        <TabsTrigger value="login">{t(`auth.login`)}</TabsTrigger>
-      </TabsList>
-      <TabsContent value="signup">
-        <Card>
-          <CardHeader>
-            <CardDescription>{t(`auth.make_changes`)}</CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">{t(`auth.name`)}</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">{t(`auth.username`)}</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>{t(`auth.save_changes`)}</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-
-      <TabsContent value="login">
-        <Card>
-          <CardHeader>
-            <CardDescription>{t(`auth.change_password`)}</CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">{t(`auth.current_password`)}</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">{t(`auth.new_password`)}</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>{t(`auth.save_password`)}</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
   );
 }
